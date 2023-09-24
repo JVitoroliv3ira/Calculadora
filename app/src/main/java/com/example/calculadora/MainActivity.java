3bd1327b7338927b60e20a08058489ef40dcf617
jvitor07 <jvitoroliveira0711@gmail.com>
2023-09-23 21:40:25 -0300

    feature: Implementa logica de troca dos fragmentos

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -1,14 +1,52 @@
 package com.example.calculadora;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.calculadora.enums.SelectedFragment;
+import com.example.calculadora.fragments.Calculadora;
+import com.example.calculadora.fragments.CalculadoraNotas;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SelectedFragment selectedFragment = SelectedFragment.CALCULADORA;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Button botaoCalculadora = findViewById(R.id.btn_calculadora);
+        final Button botaoCalculadoraNotas = findViewById(R.id.btn_calculadora_notas);
+
+        botaoCalculadora.setOnClickListener(view -> {
+            this.selecionarCalculadora();
+            this.atualizarFragmentoExibido(view);
+        });
+        botaoCalculadoraNotas.setOnClickListener(view -> {
+            this.selecionarCalculadoraNotas();
+            this.atualizarFragmentoExibido(view);
+        });
+    }
+
+    private void selecionarCalculadora() {
+        this.selectedFragment = SelectedFragment.CALCULADORA;
+    }
+
+    private void selecionarCalculadoraNotas() {
+        this.selectedFragment = SelectedFragment.CALCULADORA_NOTAS;
+    }
+
+    private void atualizarFragmentoExibido(View view) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        if (Boolean.TRUE.equals(SelectedFragment.CALCULADORA.equals(this.selectedFragment))) {
+            transaction.replace(R.id.layout, Calculadora.newInstance());
+        } else {
+            transaction.replace(R.id.layout, CalculadoraNotas.newInstance());
+        }
+        transaction.commit();
     }
 }
